82d63bc5357144aaef0c4fbfa0c50a6646bc3d91
Pierre Goutagny <pierre.goutagny@inria.fr>
2026-06-02 10:32:44 +0200

    Add showdate and tweak script

--- showlist.sh
+++ showlist.sh
@@ -1,36 +1,47 @@
-catala showlist \
+#!/bin/sh
+
+SHOW=showdate
+
+catala $SHOW \
     -I ./prologue_france \
     -I ./base_mensuelle_allocations_familiales \
     -I ./prestations_familiales \
     -I ./smic \
     -I ./allocations_familiales \
     --color=always \
+    --optimize \
     aides_logement/Aides_logement.catala_fr \
-    > aides_logement/Aides_logement.showlist
+    > aides_logement/Aides_logement.$SHOW
 
-catala showlist \
+catala $SHOW \
     -I ./prologue_france \
     -I ./base_mensuelle_allocations_familiales \
     -I ./prestations_familiales \
     -I ./smic \
     --color=always \
+    --optimize \
     allocations_familiales/Allocations_familiales.catala_fr \
-    > allocations_familiales/Allocations_familiales.showlist
+    > allocations_familiales/Allocations_familiales.$SHOW
 
-catala showlist \
+catala $SHOW \
     --color=always \
+    --optimize \
     droit_successions/droit_successions.catala_fr \
-    > droit_successions/droit_successions.showlist
+    > droit_successions/droit_successions.$SHOW
 
-catala showlist \
+catala $SHOW \
     --color=always \
+    --optimize \
     impot_revenu/impot_revenu.catala_fr \
-    > impot_revenu/impot_revenu.showlist
+    > impot_revenu/impot_revenu.$SHOW
 
-catala showlist \
+catala $SHOW \
     --color=always \
+    --optimize \
     us_tax_code/section_121.catala_en \
-    > us_tax_code/section_121.showlist
+    > us_tax_code/section_121.$SHOW
+
+exit
 
 # show nested exception depth
 ../catala/_build/default/compiler/catala.exe show_exc_depth \
